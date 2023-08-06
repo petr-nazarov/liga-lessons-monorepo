68f28c80a71993fea67e5e016e3fd0c4317ac80b
Petr Nazarov <mrzueck@gmail.com>
2023-08-06 18:31:59 +0000

    tests

--- apps/server/test/app.e2e-spec.ts
+++ apps/server/test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,9 +15,13 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close()
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/greeter/hello')
       .expect(200)
       .expect('Hello World!');
   });
