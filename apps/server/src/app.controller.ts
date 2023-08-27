6f54ac3cb5406f8df887ef42e1a4576956041ebf
Petr Nazarov <mrzueck@gmail.com>
2023-08-27 16:38:45 +0000

    feat(app): Add version to home route

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
-@Controller('greeter')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('../package.json');
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get('hello')
+  @Get('greeter/hello')
   getHello(): string {
     return 'Hello World!';
     return this.appService.getHello();
+  }
+
+  @Get()
+  index() {
+    return {
+      version: packageJson.version,
+    };
   }
 }
