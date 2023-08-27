e1d66c940828e2ce654c4c7699cebdd83e801819
Petr Nazarov <mrzueck@gmail.com>
2023-08-27 18:02:16 +0000

    feat(ci): Update ci files

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -6,12 +6,12 @@
 export APP=${1}
 export DOCKER_NAME=ghcr.io/petr-nazarov/liga-lessons-monorepo
 export TAG=latest
-export DOPPLER_TOKEN=dp.pt.ZR8sZzu0lR24urMWhBt2EbMwR2W59f7WHx7odvCnGz4
+export DOPPLER_TOKEN=dp.pt.GSM3HAFphSd4s3CvMpz9fhUjFTkxNhOlfG6PCJxUiMb
 
 # build
-docker build --platform linux/amd64 -t $DOCKER_NAME  -f ./apps/${APP}/Dockerfile .
+#docker build --platform linux/amd64 -t $DOCKER_NAME  -f ./apps/${APP}/Dockerfile .
 
-# docker run -p 8000:8000 -e DOPPLER_TOKEN=$DOPPLER_TOKEN_AFU -e APP_ENV=prod $DOCKER_NAME
-docker push $DOCKER_NAME:$TAG
+ docker run -p 8000:80 -e DOPPLER_TOKEN=$DOPPLER_TOKEN -e APP_ENV=prod -e PORT=80 $DOCKER_NAME:$TAG
+#docker push $DOCKER_NAME:$TAG
 
 
